62f82caca8f9f4167cf0186153eb2b1409061cff
noelensith <mbnoelen@gmail.com>
2022-04-28 00:51:50 -0700

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form')
+    form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildList(e.target["new-task-description"].value);
+      form.reset();
+  })
 });
+
+function buildList(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', removeTodo)
+  btn.textContent = ' x ';
+  p.textContent = `${todo} `;
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function removeTodo(e) {
+  e.target.parentNode.remove();
+}
+
